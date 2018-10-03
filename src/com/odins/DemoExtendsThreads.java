c6fc630d0d9fb0f46ed77a95e82046f1a18f8f61
Pavel Zavyalik <zavailik1365@gmail.com>
2018-10-03 18:50:39 +0300

    Заменена реализация ожидания с isAlev() на join()

--- src/com/odins/DemoExtendsThreads.java
+++ src/com/odins/DemoExtendsThreads.java
@@ -7,40 +7,17 @@ public class DemoExtendsThreads {
     public static void main(String arg[]) {
 
         ExtendsThreads threadArray[] = new ExtendsThreads[10];
-        boolean jobFinished[] = {false, false, false, false, false,
-                false, false, false, false, false};
-        boolean jobsFinished;
 
         for (int i = 0; i < 10; i++) {
             threadArray[i] = new ExtendsThreads("Поток номер: " + i);
             threadArray[i].start();
         }
 
-        while (true) {
-
-            jobsFinished = true;
-
-            for (int i = 0; i < 10; i++){
-                if (threadArray[i].isAlive()) {
-                    break;
-                }  else {
-                    jobFinished[i] = true;
-                }
-            }
-
-            for (int i = 0; i < 10; i++){
-                if (!jobFinished[i]) {
-                    jobsFinished = false;
-                }
-            }
-
-            if (!jobsFinished) {
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException exc) {
-                    System.out.println("Выполнение потока переваною");}
-            } else {
-                break;
+        for (int i = 0; i < 10; i++) {
+            try {
+                threadArray[i].join();
+            } catch (InterruptedException exc) {
+                System.out.println("Прерываение основного потока.");
             }
         }
     }
